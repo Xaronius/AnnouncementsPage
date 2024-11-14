07baa5debb2ef33f160d93e007501b90f9f6b58b
Xaronius <jakub.jan.pozorski@gmail.com>
2024-11-14 16:33:20 +0100

    example pages commit

---
+++ src/main/java/com/example/announcementspage/Controllers/controller.java
@@ -0,0 +1,26 @@
+package com.example.announcementspage.Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class controller {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "index.html"; // This maps to index.html in the templates folder
+    }
+
+    @GetMapping("/announcements")
+    public String announcementsPage() {
+        System.out.println("Success");
+        return "announcements.html"; // You may also return a JSON response if needed
+    }
+
+    @GetMapping("/CreateNew")
+    public String CreateNewPage() {
+        System.out.println("Success");
+        return "createNew.html"; // You may also return a JSON response if needed
+    }
+
+}
