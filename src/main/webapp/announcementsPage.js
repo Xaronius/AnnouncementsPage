971493b3ffc09bbf560812f4d5e7bf0308576415
Xaronius <jakub.jan.pozorski@gmail.com>
2024-11-17 13:58:40 +0100

    commit 6
    Changed profile button to adjust it as dropdown with Profile and my announcements (and additional buttons/functions if needed). Also adjusted Create new page.

--- src/main/webapp/announcementsPage.js
+++ src/main/webapp/announcementsPage.js
@@ -1,22 +1,3 @@
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('announcementForm');
-    const titleElement = form.querySelector('.AnnouncementsTitle');
-    const textElement = form.querySelector('.AnnouncementsText');
-
-    form.addEventListener('submit', function (event) {
-        // Prevent the default form submission
-        event.preventDefault();
-
-        // Extract text from <h2> and <p>
-        const title = encodeURIComponent(titleElement.textContent.trim());
-        const text = encodeURIComponent(textElement.textContent.trim());
-
-        // Redirect with query parameters
-        window.location.href = form.action + `?title=${title}&text=${text}`;
-    });
-});
-*/
 document.addEventListener('DOMContentLoaded', function () {
     // Select all forms with the class "AnnouncementsDiv"
     const forms = document.querySelectorAll('.AnnouncementsDiv');
@@ -38,3 +19,22 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+// ----------------------------- Dropdown ----------------------------- //
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropButton')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -27,3 +27,22 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+// ----------------------------- Dropdown ----------------------------- //
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropButton')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
