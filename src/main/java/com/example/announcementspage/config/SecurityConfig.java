20829ecdfaa4e4d70fddfc0e893cc64eed006166
mikolaj-kowalski <miqikow@outlook.com>
2025-01-23 22:01:32 +0100

    mk session login/logout 2

--- src/main/java/com/example/announcementspage/config/SecurityConfig.java
+++ src/main/java/com/example/announcementspage/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -73,7 +74,11 @@ public class SecurityConfig {
                         .passwordParameter("password")
                         .successHandler(customAuthenticationSuccessHandler)
                         .defaultSuccessUrl("/dashboard", true)
+                        .failureUrl("/LoginPage")
                         .permitAll()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
